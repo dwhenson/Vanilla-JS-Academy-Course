c5210bc187e5a40c4572757928025b63515ed31e
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-05-12 15:54:38 +0300

    Update Day 1 with feedback on solution proposed

---
+++ Day 1 - Toggle Password Visibility/main.js
@@ -0,0 +1,16 @@
+const checkbox = document.querySelector('[type="checkbox"]');
+const attributeToChange = document.querySelector('[type="password"]');
+
+function showHidePassword(element) {
+  if (element) {
+    // correct but better to link changes to checkbox state
+    // element.getAttribute('type') === 'password'
+    //   ? element.setAttribute('type', 'text')
+    //   : element.setAttribute('type', 'password');
+    checkbox.checked ? (element.type = 'text') : (element.type = 'password');
+  }
+}
+
+checkbox.addEventListener('change', () => {
+  showHidePassword(attributeToChange);
+});
