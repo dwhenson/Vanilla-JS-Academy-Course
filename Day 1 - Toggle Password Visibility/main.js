b8b74c81accba01c3fc35556ada101f1f51fa81e
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-05-12 16:43:58 +0300

    Commit to merge conflicts #	Day 1 - Toggle Password Visibility/main.js

--- Day 1 - Toggle Password Visibility/main.js
+++ Day 1 - Toggle Password Visibility/main.js
@@ -1,19 +1,16 @@
-const testDiv = document.getElementById('password');
-testDiv.textContent; //?
+const checkbox = document.querySelector('[type="checkbox"]');
+const attributeToChange = document.querySelector('[type="password"]');
 
-// const checkbox = document.querySelector('#show-password');
-// const attributeToChange = document.querySelector('[type="password"]');
+function showHidePassword(element) {
+  if (element) {
+    checkbox.checked ? (element.type = 'text') : (element.type = 'password');
+    // correct solution but better to link changes to checkbox state
+    // element.getAttribute('type') === 'password'
+    //   ? element.setAttribute('type', 'text')
+    //   : element.setAttribute('type', 'password');
+  }
+}
 
-// function showHidePassword(element) {
-//   if (element) {
-//     // correct but better to link changes to checkbox state
-//     // element.getAttribute('type') === 'password'
-//     //   ? element.setAttribute('type', 'text')
-//     //   : element.setAttribute('type', 'password');
-//     checkbox.checked ? (element.type = 'text') : (element.type = 'password');
-//   }
-// }
-
-// checkbox.addEventListener('change', () => {
-//   showHidePassword(attributeToChange);
-// });
+checkbox.addEventListener('change', () => {
+  showHidePassword(attributeToChange);
+});
