e229eabd7eb8ee15a78522de993ce28e49567f65
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-05-12 16:42:10 +0300

    Add folder structure for Day 2

--- Day 1 - Toggle Password Visibility/main.js
+++ Day 1 - Toggle Password Visibility/main.js
@@ -1,16 +1,19 @@
-const checkbox = document.querySelector('[type="checkbox"]');
-const attributeToChange = document.querySelector('[type="password"]');
+const testDiv = document.getElementById('password');
+testDiv.textContent; //?
 
-function showHidePassword(element) {
-  if (element) {
-    // correct but better to link changes to checkbox state
-    // element.getAttribute('type') === 'password'
-    //   ? element.setAttribute('type', 'text')
-    //   : element.setAttribute('type', 'password');
-    checkbox.checked ? (element.type = 'text') : (element.type = 'password');
-  }
-}
+// const checkbox = document.querySelector('#show-password');
+// const attributeToChange = document.querySelector('[type="password"]');
 
-checkbox.addEventListener('change', () => {
-  showHidePassword(attributeToChange);
-});
+// function showHidePassword(element) {
+//   if (element) {
+//     // correct but better to link changes to checkbox state
+//     // element.getAttribute('type') === 'password'
+//     //   ? element.setAttribute('type', 'text')
+//     //   : element.setAttribute('type', 'password');
+//     checkbox.checked ? (element.type = 'text') : (element.type = 'password');
+//   }
+// }
+
+// checkbox.addEventListener('change', () => {
+//   showHidePassword(attributeToChange);
+// });
