d36d9e6dd12fd9eb0e7d06ad393a38940c73fba9
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-05-15 13:21:17 +0300

    Update with alternative solution

--- Day 3 - Toggle multiple fields/main.js
+++ Day 3 - Toggle multiple fields/main.js
@@ -1,11 +1,33 @@
+/*=================================
+=            Variables            =
+=================================*/
+
 const passwordInput = Array.from(
   document.querySelectorAll('[type="password"]'));
 const checkbox = document.querySelector('#show-passwords');
 
-function toggleVisability() {
-  passwordInput.forEach((input) => {
-    checkbox.checked ? (input.type = 'text') : (input.type = 'password');
-  });
+/*=================================
+=            Functions            =
+=================================*/
+
+/**
+ * toggle 'type' attribute of single password input field
+ * @param  {element} password The input field
+ */
+function toggglePassword(input) {
+  checkbox.checked ? (input.type = 'text') : (input.type = 'password');
 }
 
-checkbox.addEventListener('change', toggleVisability);
+/**
+ * toggle visibility of all inputs fields in an array
+ */
+function togglePasswords() {
+  passwordInput.forEach(toggglePassword);
+}
+
+/* =======================================
+=            Event listeners            =
+======================================= */
+
+// toggle visibility of passwords by changing 'type' attr on 'change'
+checkbox.addEventListener('change', togglePasswords);
