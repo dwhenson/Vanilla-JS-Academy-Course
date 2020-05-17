c9bbb73cbb44e4dc68a13778ea8c8a677f5fe23c
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-05-17 17:41:53 +0300

    Add solution Day 7 - Character count

--- Day 5 - Toggle in multiple forms/main.js
+++ Day 5 - Toggle in multiple forms/main.js
@@ -7,10 +7,26 @@ document.addEventListener('click', (event) => {
     // NB html edited to adjust name attr to just 'password'
     const passwordInputs = [...form.querySelectorAll('[name="password"]')];
     // loop over array of password inputs and change 'type' attr as needed
-    console.log(passwordInputs);
     passwordInputs.forEach((input) => {
       event.target.checked ? (input.type = 'text') : (input.type = 'password');
     });
   }
 });
 
+document.addEventListener('click', (event) => {
+  if (!event.target.matches('[data-pw-toggle]')) return;
+  // create an array from node list
+  const passwords = Array.from(
+    // select all elements based on the attr values returned from QSA
+    document.querySelectorAll(
+      // get attr values from event target
+      // NB must attr values must match those on elements to be selected
+      event.target.getAttribute('[data-pw-toggle]'),
+    ),
+  );
+  passwords.forEach((password) => {
+    event.target.checked
+      ? (password.type = 'text')
+      : (password.type = 'password');
+  });
+});
