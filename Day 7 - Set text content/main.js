2111cb388f3d9ff6adcc4b039ebfdb9987d0ea7f
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-05-17 20:03:12 +0300

    Update solution to avoid anon function

--- Day 7 - Set text content/main.js
+++ Day 7 - Set text content/main.js
@@ -1,6 +1,8 @@
 const text = document.querySelector('#text');
 const characterCount = document.querySelector('#character-count');
 
-text.addEventListener('input', () => {
-  characterCount.textContent = text.value.length;
-});
+function characterCounter(element) {
+  element.textContent = text.value.length;
+}
+
+text.addEventListener('input', characterCounter(null, characterCount));
