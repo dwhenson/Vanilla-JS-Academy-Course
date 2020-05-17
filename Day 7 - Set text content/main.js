7dc1be0961b1ac007dd4bcc11ec6b81cfc49f517
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-05-17 20:42:28 +0300

    Updated to include both options

--- Day 7 - Set text content/main.js
+++ Day 7 - Set text content/main.js
@@ -1,6 +1,8 @@
 const text = document.querySelector('#text');
 const characterCount = document.querySelector('#character-count');
 
+// Alternative without anonymous function
+
 function characterCounter(element) {
   element.textContent = text.value.length;
 }
