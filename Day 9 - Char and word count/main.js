368221c5b470bb8a7dbb01fa3d01532741e70a88
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-05-20 07:47:57 +0300

    Final Day 9 working solution

--- Day 9 - Char and word count/main.js
+++ Day 9 - Char and word count/main.js
@@ -1,17 +1,14 @@
 const inputArea = document.querySelector('#text');
 const numberChars = document.querySelector('#character-count');
 const numberWords = document.querySelector('#word-count');
+const seperators = (/[\s\n]+/);
 
 function handleCharCount(element) {
   element.textContent = inputArea.value.length;
 }
 
-// split on space
-// filter if great than 0
-// return length to element.textContent
-
 function handleWordCount(element) {
-  const wordArray = inputArea.value.split(' ').filter(function (word) {
+  const wordArray = inputArea.value.split(seperators).filter(function (word) {
     return word.length > 0;
   });
   element.textContent = wordArray.length;
