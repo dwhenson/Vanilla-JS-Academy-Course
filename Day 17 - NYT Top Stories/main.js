b0beb3db31e7edc0f95f88a023079d8eef3f6d5d
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-05-27 10:26:47 +0300

    First working solution with forEach (no links)

--- Day 17 - NYT Top Stories/main.js
+++ Day 17 - NYT Top Stories/main.js
@@ -1,17 +1,19 @@
 // It should get the top stories from the NYT website, and then display them on a webpage
 // Steps:
-// Set variables for APIendpoint, and app
-// Fetch top stories from NYT - check return is array
-// Store array in variable?
-// Map over array adding li to each end (and ul overall)
+// Set variables for APIendpoint, and app  - DONE
+// Fetch top stories from NYT - check return is array - DONE
+// Store array in variable - parameter, sufficient?
+// Map over array extracting abstract and li to each end (and ul overall)
+// Add links to each abstract li element
 // Update app innerHTML with new array created from map
-// 
+//
+// Further work, e
 
 /* ==========  variables  ========== */
 
-const endpoint = 'https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=IVkW1nLsL3ufkeo8FjJrw6oXLUo7qZ62';
+const endpoint =
+	'https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=IVkW1nLsL3ufkeo8FjJrw6oXLUo7qZ62';
 const app = document.querySelector('#app');
-
 
 /* ==========  functions  ========== */
 
@@ -19,24 +21,43 @@ function convertJSON(response) {
 	return response.ok ? response.json() : Promise.reject(response);
 }
 
-function displayStories (storyArray) {
-	// TODO display in html
-	console.log(storyArray); 
+//
+
+// function displayStories(responseJSON) {
+// 	const storyArray = responseJSON.results;
+// 	storyArray.forEach( function(element, index) {
+// 		console.log(element[index].url);
+// 	});
+// 	// storyArray.map(function (story, index) {
+// 	// 	return;
+// 	// 	`
+// 	// 	<li>
+// 	// 	<a href="${storyArray[index].url}">
+// 	// 	${storyArray[index].abstract}
+// 	// 	</li>
+// 	// 	`;
+// 	// });
+// 	// app.innerHTML = storyArray;
+// }
+
+function displayStories(responseJSON) {
+	let html = '';
+	const storyArray = responseJSON.results;
+	storyArray.forEach(function (story, index) {
+		html += '<li>' + storyArray[index].abstract + '</li>';
+	});
+	app.innerHTML = '<ul>' + html + '</ul>';
 }
 
 function catchError(error) {
-	// TODO display in html 
-	console.log('Ohhh no!', error);
+	// TODO display in html
+	console.log('Oh no!', error);
 }
 
-
-function fetchStories (APIendpoint) {
-	fetch(APIendpoint)
-	.then(convertJSON)
-	.then(displayStories)
-	.catch(catchError)
+function fetchStories(APIendpoint) {
+	fetch(APIendpoint).then(convertJSON).then(displayStories).catch(catchError);
 }
 
 /* ==========  execution  ========== */
 
-fetchStories(endpoint)
+fetchStories(endpoint);
