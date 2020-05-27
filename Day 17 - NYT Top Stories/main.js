080863a35a3fbbe28893f4bac3db6a882cbe88bd
Dave Henson <50716370+dwhenson@users.noreply.github.com>
2020-05-27 17:39:19 +0300

    Final edits and formatting

--- Day 17 - NYT Top Stories/main.js
+++ Day 17 - NYT Top Stories/main.js
@@ -1,3 +1,5 @@
+// Check API documentation for updating
+
 /* ==========  variables  ========== */
 
 const app = document.querySelector('#app');
@@ -8,8 +10,8 @@ const endpoint =
 
 /**
  * converts response from an API to a JSON object
- * @param  {string} (response) Unprocessed response from request
- * @return {object}           Response converted to JSON or rejected promise
+ * @param  {string} (response)	Unprocessed response from request
+ * @return {object}           	Response converted to JSON or rejected promise
  */
 function convertJSON(response) {
 	return response.ok ? response.json() : Promise.reject(response);
@@ -17,14 +19,14 @@ function convertJSON(response) {
 
 /**
  * finds desired data on an array of objects, map to string and insert
- * @param  {object} (responseJSON) response from fetch request in JSON form
- * @return {string} desired values 'mapped' out and inserted into HTML
+ * @param  {object} (responseJSON)	response from fetch request in JSON form
+ * @return {string} 								values 'mapped' out and inserted into HTML
  */
 function displayStories(responseJSON) {
 	const storyList = responseJSON.results
 		.map(function (story) {
 			return `
-			<li><a href="${story.url}">${story.title}</a></li>
+			<li><b><a href="${story.url}">${story.title}</a></b></li>
 			<p>${story.abstract}<b>
 			<br>${story.byline}</b><i>&nbsp(${story.published_date})</i></p>`;
 		})
@@ -33,10 +35,11 @@ function displayStories(responseJSON) {
 }
 
 /**
- * catch and present error if fetch request != 'OK'
+ * catch and present error if fetch request is not 'OK'
  */
 function catchError(error) {
-	app.innerHTML = `<p>I'm sorry we can't retrieve any suggestions at the moment.<br>The New York Times has some good suggestions <a href="https://www.nytimes.com">here</a></p>`;
+	app.innerHTML = `
+		<p>I'm sorry we can't retrieve any suggestions at the moment.<br>The New York Times has some good suggestions <a href="https://www.nytimes.com">here</a></p>`;
 }
 
 function fetchStories(APIendpoint) {
