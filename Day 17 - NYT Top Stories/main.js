622e5c5c8dd8ab9fad054c021cfc229df5b87652
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-05-27 13:11:06 +0300

    Add alternate working solution working with map()

--- Day 17 - NYT Top Stories/main.js
+++ Day 17 - NYT Top Stories/main.js
@@ -1,13 +1,5 @@
-// It should get the top stories from the NYT website, and then display them on a webpage
-// Steps:
-// Set variables for APIendpoint, and app  - DONE
-// Fetch top stories from NYT - check return is array - DONE
-// Store array in variable - parameter, sufficient?
-// Map over array extracting abstract and li to each end (and ul overall)
-// Add links to each abstract li element
-// Update app innerHTML with new array created from map
-//
-// Further work, e
+// Sort out error message - should array be global variable?
+// Check API documentation for updating
 
 /* ==========  variables  ========== */
 
@@ -21,41 +13,36 @@ function convertJSON(response) {
 	return response.ok ? response.json() : Promise.reject(response);
 }
 
-//
-
-// function displayStories(responseJSON) {
-// 	const storyArray = responseJSON.results;
-// 	storyArray.forEach( function(element, index) {
-// 		console.log(element[index].url);
-// 	});
-// 	// storyArray.map(function (story, index) {
-// 	// 	return;
-// 	// 	`
-// 	// 	<li>
-// 	// 	<a href="${storyArray[index].url}">
-// 	// 	${storyArray[index].abstract}
-// 	// 	</li>
-// 	// 	`;
-// 	// });
-// 	// app.innerHTML = storyArray;
-// }
-
 function displayStories(responseJSON) {
-	let html = '';
 	const storyArray = responseJSON.results;
-	storyArray.forEach(function (story, index) {
-		html += '<li>' + storyArray[index].abstract + '</li>';
-	});
-	app.innerHTML = '<ul>' + html + '</ul>';
+	app.innerHTML = storyArray
+		.map(function (story) {
+			return `<li><a href="${story.url}">${story.abstract}</a></li>`;
+		})
+		.join('');
 }
 
+// function displayStories(responseJSON) {
+// 	let html = '';
+// 	const storyArray = responseJSON.results;
+// 	storyArray.forEach(function (story, index) {
+// 		// CHECK why storyArray[index] and not story??
+// 		// NOTE because storyArray[index] = story (not story[index])
+// 		html += '<li>' + storyArray[index].abstract + '</li>';
+// 	});
+// 	app.innerHTML = '<ul>' + html + '</ul>';
+// }
+
 function catchError(error) {
-	// TODO display in html
+	app.innerHTML = `<p>I'm sorry we can't retrieve any suggestions at the moment.<br>The New York Times has some good suggestions <a href="https://www.nytimes.com">here</a></p>`;
 	console.log('Oh no!', error);
 }
 
 function fetchStories(APIendpoint) {
-	fetch(APIendpoint).then(convertJSON).then(displayStories).catch(catchError);
+	fetch(APIendpoint)
+		.then(convertJSON) //
+		.then(displayStories)
+		.catch(catchError);
 }
 
 /* ==========  execution  ========== */
