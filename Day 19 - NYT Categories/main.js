cdc68369d3abdc877179cbc42ff63e58abaadaf0
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-05-30 09:28:39 +0300

    Undo big git mess

--- Day 19 - NYT Categories/main.js
+++ Day 19 - NYT Categories/main.js
@@ -1,39 +1,31 @@
-// Get articles from categories, then render the top three from API data into markup and inject it into the #app
-//
-// add a delegate event listener to option elements, on change? get event.target.value - how to store/extract - set as variable?
-// insert event.target.value into place holder APIend point, and call fetch based on this updated value (endpoint, 'category', and APIkey)
-// get returned array and trim so that only the first top five stories are presented (.array(slice)
-// map sliced array to generate html and present in app.inner html - include a heading before each section showing which section they are from
-//
-//
-
 /* ==========  variables  ========== */
 
 const dropdown = document.querySelector('#topics');
 const app = document.querySelector('#app');
-const placeholder = document.querySelector('#placeholder');
 const heading = document.querySelector('#heading');
+const placeholder = document.querySelector('#placeholder');
 const apiKey = `MzjNjEmTGPTcAbKdbZonokosBAmd42Xd`;
-let endpoint = '';
+const placeholderText = `One moment please...loading your articles`;
+const numberArticles = 5;
+let endpoint;
 
 /* ==========  functions  ========== */
-
 
 /**
  * Adds placeholder text while fetch is working
  * @return {string} text displayed in target element
  */
-function generatePlaceholder() {
-	placeholder.textContent = `One moment please...loading your articles`;
+function generatePlaceholder(text) {
+	placeholder.textContent = text;
 }
 
 /**
- * Creates APIendpoint based on option selected
- * @param  {string} category 	Value from event target selected
+ * Creates APIendpoint based on option selected 
+ * @param  {string} category 	Value from event target selected			
  * @return {string}						Completed API endpoint
  */
-function createEndpoint(category) {
-	endpoint = `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=`;
+function createEndpoint(topic) {
+	endpoint = `https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=`;
 	return endpoint;
 }
 
@@ -42,22 +34,15 @@ function createEndpoint(category) {
  * @param  {string} category Value from event target selected
  * @return {string}          HTML inserted into target element
  */
-<<<<<<< HEAD
-function generateHeading(topic) {
-	return new Promise(function (resolve) {
-		resolve(
-			(heading.innerHTML = `Top ${numberArticles} Articles About ${topic}`)
-		);
-	});
-=======
-function generateHeading (category) {
-	heading.innerHTML = `Top Five Articles About ${category}`
->>>>>>> parent of 4f50024... Final refactored solution
+function generateHeading (topic) {
+	heading.innerHTML = `Top ${numberArticles} Articles About ${topic}`
 }
+
+
 
 /**
  * Converts response from an API to a JSON object
- * @param  {string} Response 	Unprocessed response from request
+ * @param  {string} response 	Unprocessed response from request
  * @return {object} 					Response converted to JSON or rejected promise
  */
 function convertJSON(response) {
@@ -71,33 +56,18 @@ function convertJSON(response) {
  * @return {string} 					Articles 'mapped' out from array into HTML format
  */
 function render(element, articles) {
-<<<<<<< HEAD
-	return new Promise(function (resolve) {
-		resolve(
-			(element.innerHTML = articles.results
-				.slice(0, numberArticles)
-				.map(function (article) {
-					return `
-			<article>
-			<h3><a href="${article.url}">${article.title}</a></h3>
-			<i>Published on ${article.published_date.slice(0, 10)}</i>
-			<p>${article.abstract}
-			<br><b>${article.byline}</b></p>
-=======
 	element.innerHTML = articles.results
-		.slice(0, 5)
+		.slice(0, numberArticles)
 		.map(function (article) {
 			return `
 			<article>
 			<h3><a href="${article.url}">${article.title}</a></h3>
-			<p>${article.abstract}<b>
-			<br>${article.byline}</b><i>&nbsp(${article.published_date})</i></p>
->>>>>>> parent of 4f50024... Final refactored solution
+			<i>Published on ${article.published_date.slice(0,10)}</i>
+			<p>${article.abstract}
+			<br><b>${article.byline}</b></p>
 			</article>`;
-				})
-				.join(''))
-		);
-	});
+		})
+		.join('');
 }
 
 /**
@@ -105,7 +75,8 @@ function render(element, articles) {
  */
 function catchError(error) {
 	app.innerHTML = `
-		<p>I'm sorry we can't retrieve any suggestions at the moment.<br><a href="https://www.nytimes.com">The New York Times has some good ideas though</a></p>`;
+		<p>I'm sorry we can't retrieve any suggestions at the moment.<br>
+		<a href="https://www.nytimes.com">The New York Times</a> has some good ideas though</p>`;
 }
 
 /**
@@ -124,15 +95,9 @@ function fetchStories(APIendpoint) {
 /* ==========  execution  ========== */
 
 dropdown.addEventListener('change', (event) => {
-<<<<<<< HEAD
 	const topic = event.target.value;
 	generatePlaceholder(placeholderText);
 	createEndpoint(topic);
-	Promise.all([generateHeading(topic), fetchStories(endpoint)]);
-=======
-	generatePlaceholder();
-	createEndpoint(event.target.value);
-	generateHeading(event.target.value)
+	generateHeading(topic)
 	fetchStories(endpoint);
->>>>>>> parent of 4f50024... Final refactored solution
 });
