7c7e96069575582988a48ef3d1655856681f92e4
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-06-10 08:43:29 +0300

    Set up Day 51 structure

--- Day 31 - Missing IDs/main.js
+++ Day 31 - Missing IDs/main.js
@@ -1,3 +1,18 @@
+// GOAL
+// create table of contents,
+// if the heading doesn’t have an ID to link to
+// create one and assign it to the heading.
+// 
+// STEPS
+// for each item in array
+// * array.forEach (item => function)
+// check if ID exists
+// * function (if id return, else)
+// if no ID create attribute where ID is the title
+// * item.setAttribute (id, ${item.textContent})
+// insert in HTML
+// * pass to createToc?
+
 /* ==========  Variables  ========== */
 
 const toc = document.querySelector('#table-of-contents');
