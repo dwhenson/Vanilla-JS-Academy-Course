96ab37db5318c8963102f63a1c7611ebe3976c0b
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-06-14 08:49:09 +0300

    Add Day 35 files and structure

---
+++ Day 35 - Weather App/main.js
@@ -0,0 +1,25 @@
+// GOAL Build an app that gets a user’s location and displays their current weather information (use ipapi for location and Weatherbit for weather data) 
+// 
+// STEPS 
+// fetch the location information from ipai
+// use object.city to fetch weather information from weather bit
+// render to app
+// figure out icons...
+// 
+// Create vars for app, location and weather-api, and weather(?)
+// Fetch() location information and set as location variable
+// Then() fetch weather-api + ${location} - in correct place
+// Render variable to app - aria-live
+// Figure out associated icons...
+
+
+
+// avoid global scope
+(function () {
+
+/* ==========  Variables  ========== */
+
+const weatherAPI = 'c81e60446f394ac3b6efb4b5c187cafa';
+
+// close scope
+})();
