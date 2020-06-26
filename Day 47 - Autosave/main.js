efd24b497c387c0148147d51e0b78188e888ae4c
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-06-26 20:14:12 +0300

    Day 47 first working solution

--- Day 47 - Autosave/main.js
+++ Day 47 - Autosave/main.js
@@ -1,9 +1,40 @@
-// GOAL As the user types, the value of a form field should automatically be saved. 
+// GOAL As the user types, the value of a form field should automatically be saved.
 // When the page is reloaded or opened back up, the form should be repopulated with the user’s saved data.
 // If the form is submitted, clear the data from storage and wipe the form.
-// 
-// STEPS:
-// Delegate event listener to form
-// On change, get input value
-// Save input value to local storage
-// On submit clear all local storage
+//
+
+// TODO
+// Clear data on submit
+// Convert for loop to for of loop or something?
+// add in email verification in HTML?
+
+// Avoid global scope
+(function () {
+  /* ==========  Variables  ========== */
+
+  const form = document.querySelector('#save-me');
+  const submit = document.querySelector('button')
+
+  /* ==========  Functions  ========== */
+
+  function setStorage(event) {
+    localStorage.setItem(event.target.name, event.target.value);
+  }
+
+  function getStorage() {
+    for (var i = 0; i < localStorage.length; i++) {
+      const content = document.querySelector(`#${localStorage.key(i)}`);
+      content.value = localStorage.getItem(localStorage.key(i));
+    }
+  }
+
+  function removeStorage () {
+    localStorage.clear();
+  }
+
+  /* ==========  Event listeners and inits  ========== */
+
+  form.addEventListener('input', setStorage);
+  window.addEventListener('load', getStorage);
+  submit.addEventListener('click', removeStorage);
+})();
