9b3f524ac56b8237f6a0f2acaa904f1b66954c38
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-06-27 14:36:54 +0300

    Revert "Updated solution with replaced loop"
    This reverts commit 65617f35f8f7f0b06d0857d74a3fa76a61ba26ee.

--- Day 47 - Autosave/main.js
+++ Day 47 - Autosave/main.js
@@ -1,15 +1,28 @@
+<<<<<<< HEAD
 // TODO 
 // change button event to submit
 // check values exist before action
 // Make less destructive:
 // localstorage.clear() - replace with remove? (prefix to form variables?)
 
+=======
+// GOAL As the user types, the value of a form field should automatically be saved.
+// When the page is reloaded or opened back up, the form should be repopulated with the user’s saved data.
+// If the form is submitted, clear the data from storage and wipe the form.
+//
+
+// TODO
+// Clear data on submit
+// Convert for loop to for of loop or something?
+// add in email verification in HTML?
+>>>>>>> parent of 65617f3... Updated solution with replaced loop
 
 // Avoid global scope
 (function () {
   /* ==========  Variables  ========== */
 
   const form = document.querySelector('#save-me');
+<<<<<<< HEAD
   const submit = document.querySelector('button');
   const storagePrefix = 'form-autosave_'
 
@@ -20,24 +33,24 @@
    */
   function setStorage() {
     localStorage.setItem(storagePrefix + event.target.name, event.target.value);
+=======
+  const submit = document.querySelector('button')
+
+  /* ==========  Functions  ========== */
+
+  function setStorage(event) {
+    localStorage.setItem(event.target.name, event.target.value);
+>>>>>>> parent of 65617f3... Updated solution with replaced loop
   }
 
-  /**
-   * Gets the values from local storage and returns them to the form
-   * @return {string} The values the user entered originally
-   */
   function getStorage() {
-    const storage = Object.entries(localStorage);
-    for (const userData of storage) {
-      const input = document.querySelector(`#${userData[0]}`);
-      input.value = localStorage.getItem(userData[0]);
+    for (var i = 0; i < localStorage.length; i++) {
+      const content = document.querySelector(`#${localStorage.key(i)}`);
+      content.value = localStorage.getItem(localStorage.key(i));
     }
   }
 
-  /**
-   * Removes all values from local storage
-   */
-  function removeStorage() {
+  function removeStorage () {
     localStorage.clear();
   }
 
