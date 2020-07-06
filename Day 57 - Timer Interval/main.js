eef1a1c428fbdf8a5ec9595c6cd1c95e814f3b37
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-07-06 14:13:21 +0300

    Add Day 57 files and set up

---
+++ Day 57 - Timer Interval/main.js
@@ -0,0 +1,2 @@
+// GOAL: Use state-based UI to create the timer, and countdown from 60 seconds to 0 once a second. When the timer is done, stop the countdown and provide a way for users to start it again.
+// 
