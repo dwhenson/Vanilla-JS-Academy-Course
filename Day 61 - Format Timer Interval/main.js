5b581f50a8d68d0fef0ba44876c2198aca78c8a2
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-07-13 09:27:29 +0300

    Day 61 Final working solution
    Based on CF soluition and lesson

--- Day 61 - Format Timer Interval/main.js
+++ Day 61 - Format Timer Interval/main.js
@@ -4,8 +4,6 @@
   const duration = 120;
   const interval = 1000;
   let timer;
-  let state = 1;
-  let action = 'Start';
 
   /* ==========  Constructor  ========== */
   const Timer = function (selector, options) {
@@ -23,74 +21,81 @@
   function getTimerHTML(props) {
     const mins = parseInt(props.time / 60);
     const secs = props.time % 60;
-    return `${mins}:${secs.toString().padStart(2, '0')}`;
+    const html = `${mins.toString()}:${secs.toString().padStart(2, '0')}
+    <p>
+    ${
+      props.paused
+        ? `<button data-start-timer>Start</button>`
+        : `<button data-pause-timer>Pause</button>`
+    }
+    </p>
+    <button class="numbers" data-restart-timer>Restart</button>`;
+    return html;
   }
 
   function stopTimer() {
-    if (app.data.time > 0) return;
     clearInterval(timer);
   }
 
   function countDown() {
     app.data.time--;
-    stopTimer();
+    if (app.data.time < 1) {
+      stopTimer();
+    }
     app.render();
   }
 
-  function startTimer() {
-    clearTimeout(timer);
+  function startTimer(event) {
+    if (!event.target.hasAttribute('data-start-timer')) return;
+
+    if (app.data.time < 1) {
+      restartTimer();
+      return;
+    }
+    app.data.paused = false;
+    app.render();
+    stopTimer();
+    timer = setInterval(countDown, interval);
+  }
+
+  function pauseTimer(event) {
+    if (!event.target.hasAttribute('data-pause-timer')) return;
+    stopTimer();
+    app.data.paused = true;
+    app.render();
+  }
+
+  function restartTimer(event) {
+    if (!event.target.hasAttribute('data-restart-timer')) return;
+    stopTimer();
     app.data.time = duration;
+    app.data.paused = false;
     app.render();
     timer = setInterval(countDown, interval);
   }
 
-  function pauseTimer() {
-    if (app.data.time === duration) {
-      startTimer();
-      action = 'Pause';
-      return;
-    }
-    if (state === 1) {
-      state = 0;
-      action = 'Start';
-      clearInterval(timer);
-      app.render();
-      return;
-    }
-    if (state === 0) {
-      state = 1;
-      action = 'Pause';
-      timer = setInterval(countDown, interval);
-      app.render();
-    }
-  }
-
   function clickHandler(event) {
-    if (event.target.hasAttribute('data-restart-timer')) {
-      startTimer();
-    }
-    if (event.target.hasAttribute('data-pause-timer')) {
-      pauseTimer();
-    }
+    startTimer(event);
+    pauseTimer(event);
+    restartTimer(event);
   }
 
   /* ==========  Instance  ========== */
   const app = new Timer('#app', {
     data: {
       time: duration,
+      paused: true,
     },
     template(props) {
-      return `<p>${getTimerHTML(props)}</p>
-      <button class="numbers" data-restart-timer>Restart</button>
-      <button class="numbers" data-pause-timer>${action}</button>`;
+      if (props.time < 1) {
+        return '⏰ <p><button data-restart-timer>Restart Timer</button></p>';
+      }
+      return getTimerHTML(props);
     },
   });
-
 
   /* ==========  Inits and Event listeners  ========== */
 
   app.render();
   document.addEventListener('click', clickHandler);
 })();
-
-
