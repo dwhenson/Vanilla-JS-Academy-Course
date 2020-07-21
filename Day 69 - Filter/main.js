4ef1b8623df2d49b1e9569c0511dada20139d6d4
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-07-21 14:58:41 +0300

    Day 70 solved set and get visited local storage working

--- Day 69 - Filter/main.js
+++ Day 69 - Filter/main.js
@@ -3,13 +3,14 @@
 // When the user selects one of the filters, change the UI to only show locations that match the filter criteria
 //
 // STEPS:
-
+//
 
 
 // avoid global scope
 (function () {
   const endpoint = 'https://vanillajsacademy.com/api/places.json';
   const favesID = 'favorites';
+  const visitID = 'visited';
 
   function getFaves() {
     const faves = localStorage.getItem(favesID);
@@ -21,14 +22,25 @@
     localStorage.setItem(favesID, JSON.stringify(faves));
   }
 
+  function getVisits() {
+    const visits = localStorage.getItem(visitID);
+    const visitObj = visits ? JSON.parse(visits) : {};
+    return visitObj;
+  }
+
+  function saveVisits(visits) {
+    localStorage.setItem(visitID, JSON.stringify(visits));
+  }
+
   function getPlaces() {
     fetch(endpoint)
       .then((response) =>
         response.ok ? response.json() : Promise.reject(response)
       )
       .then((data) => {
-        app.data.faves = getFaves();
         app.data.posts = data;
+        app.data.faves = getFaves();
+        app.data.visted = getVisits();
       })
       .catch((error) => {
         console.warn(error);
@@ -41,7 +53,8 @@
       .map(function (prop) {
         return `
         <h2>
-        <button data-fave="${prop.id}" aria-label="Save ${prop.place}" aria-pressed="${props.faves[prop.id]}">❤</button>
+        <button data-fave="${prop.id}" aria-label="Save ${prop.place}" aria-pressed="${props.faves[prop.id]}">&#10084;</button>
+        <button data-visit="${prop.id}" aria-label="Visted ${prop.place}" aria-pressed="${props.visted[prop.id]}">&#10004;</button>
         <a href="${prop.url}">${prop.place}</a>
         </h2>
         <img src="${prop.img}" alt="" width="640" height="427">
@@ -65,18 +78,28 @@
     },
   });
 
-  function clickHandler(event) {
-    const place = event.target.getAttribute('data-fave');
-    if (!place) return;
-    app.data.faves[place] = app.data.faves[place] ? false : true;
-  }
+
+ function clickHandlerFavorite(event) {
+   const place = event.target.getAttribute('data-fave');
+   if (!place) return;
+   app.data.faves[place] = app.data.faves[place] ? false : true;
+ }
+
+
+ function clickHandlerVisited(event) {
+   const place = event.target.getAttribute('data-visit');
+   if (!place) return;
+   app.data.visted[place] = app.data.visted[place] ? false : true;
+ }
 
   function renderHandler(event) {
     saveFaves(app.data.faves);
+    saveVisits(app.data.visted);
   }
 
   getPlaces();
   document.addEventListener('render', renderHandler);
-  document.addEventListener('click', clickHandler);
+  document.addEventListener('click', clickHandlerVisited);
+  document.addEventListener('click', clickHandlerFavorite);
 })();
 
